270778b2de34072b7f703a1484daa9484a7c099a
akrmyuc <87920616+akrmyuc@users.noreply.github.com>
2022-04-18 13:24:50 +0900

    update: 入力内容をコンソールに表示するよう変更

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 import { App } from "./src/App.js";
 const app = new App();
+// Appクラスのmountメソッドを呼び出す
+app.mount();
--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-console.log("App.js: loaded");
 export class App {
-    constructor() {
-        console.log("App initialized");
+    mount() {
+        const formElement = document.querySelector("#js-form");
+        const inputElement = document.querySelector("#js-form-input");
+        formElement.addEventListener("submit", (event) => {
+            // submitイベントの本来の動作を止める
+            event.preventDefault();
+            console.log(`入力欄の値: ${inputElement.value}`);
+        });
     }
 }
