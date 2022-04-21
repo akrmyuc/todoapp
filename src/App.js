812db89c091fcb36a6e9af57c1e9ae13a6d607ff
akrmyuc <87920616+akrmyuc@users.noreply.github.com>
2022-04-21 09:16:13 +0900

    update: Todoリストにアイテムを追加できるよう変更

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { element, render } from "./view/html-util.js";
+
 export class App {
     mount() {
         const formElement = document.querySelector("#js-form");
         const inputElement = document.querySelector("#js-form-input");
+        const containerElement = document.querySelector("#js-todo-list");
+        const todoItemCountElement = document.querySelector("#js-todo-count");
+        // TodoリストをまとめるList要素
+        const todoListElement = element`<ul />`;
+        // Todoアイテム数
+        let todoItemCount = 0;
         formElement.addEventListener("submit", (event) => {
-            // submitイベントの本来の動作を止める
+            // 本来のsubmitイベントの動作を止める
             event.preventDefault();
-            console.log(`入力欄の値: ${inputElement.value}`);
+            // 追加するTodoアイテムの要素(li要素)を作成する
+            const todoItemElement = element`<li>${inputElement.value}</li>`;
+            // TodoアイテムをtodoListElementに追加する
+            todoListElement.appendChild(todoItemElement);
+            // コンテナ要素の中身をTodoリストをまとめるList要素で上書きする
+            render(todoListElement, containerElement);
+            // Todoアイテム数を+1し、表示されてるテキストを更新する
+            todoItemCount += 1;
+            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+            // 入力欄を空文字列にしてリセットする
+            inputElement.value = "";
         });
     }
 }
